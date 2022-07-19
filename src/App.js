7816d9ee1dead415b2ead9a62fe1b0680af7c795
sayaksen5 <80758101+sayaksen5@users.noreply.github.com>
2022-07-19 16:54:25 +0530

    Added App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Loading from "./Views/Loading";
+import { useState, useEffect } from "react";
+import Pagination from "./Components/Features/Pagination";
 
+import Error from "./Views/Error";
+import Searchbar from "./Components/Features/Searchbar";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [loading, setLoading] = useState(true);
+  const [star, setStars] = useState("");
+  const [errors, setErrors] = useState(false);
+  const [errorDetails, setErrorDetails] = useState("");
+
+  let url = "https://swapi.dev/api/people/";
+
+  useEffect(() => {
+    const dataListing = async () => {
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+        data ? setStars(data) : setStars(star);
+      } catch (error) {
+        setErrors(true);
+        setErrorDetails(error.message);
+      }
+    };
+    dataListing();
+
+    setLoading(false);
+  }, [url]);
+
+  if (loading) {
+    return <Loading />;
+  } else {
+    return (
+      <div className="container">
+        <div>
+          <header
+            style={{ backgroundColor: "blue", height: "60px" }}
+            className="header"
+          >
+            Star Wars Heroes
+          </header>
+          <div>
+            <Searchbar star={star} />
+          </div>
+
+          <Pagination
+            count={star.count}
+            setStars={setStars}
+            star={star}
+            setLoading={setLoading}
+            url={url}
+          />
+        </div>
+        {errors ? <Error error={errorDetails} /> : ""}
+      </div>
+    );
+  }
 }
 
 export default App;
