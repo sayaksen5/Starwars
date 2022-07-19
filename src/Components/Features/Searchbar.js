1be8b98c5a603e395fbb5714a4be5864f4a0e0e7
sayaksen5 <80758101+sayaksen5@users.noreply.github.com>
2022-07-19 17:01:56 +0530

    Create Searchbar.js

---
+++ src/Components/Features/Searchbar.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import SearchResult from "../../Views/SearchResult";
+
+const Searchbar = ({ star }) => {
+  const [search, setSearch] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+  const [searched, setSearched] = useState("");
+  let result;
+  const valueSetter = (e) => {
+    const searchValue = e.target.value;
+    setSearch(searchValue);
+  };
+
+  const searchResults = () => {
+    setSubmitted(true);
+    result = star.results.filter((item) => item.name === search);
+
+    setSearched(result);
+    setSearch("");
+  };
+
+  return (
+    <div>
+      <div>
+        <input
+          value={search}
+          onChange={valueSetter}
+          placeholder="type your Starwars hero"
+          className="search"
+        ></input>
+        <button onClick={searchResults} className="search-btn">
+          Search
+        </button>
+      </div>
+      {submitted && searched.length !== 0 ? (
+        <SearchResult searched={searched} />
+      ) : (
+        ""
+      )}
+      <div>
+        {submitted && searched.length === 0 ? "Oops Nothing found" : ""}
+      </div>
+    </div>
+  );
+};
+
+export default Searchbar;
